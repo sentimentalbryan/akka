6bf65815608c02073521aacf06958fc9a798519c
Viktor Klang <viktor.klang@gmail.com>
2012-08-01 13:48:18 +0200

    Adding OnCompleteRunnable to BatchingExecutor

--- akka-actor/src/main/scala/akka/dispatch/BatchingExecutor.scala
+++ akka-actor/src/main/scala/akka/dispatch/BatchingExecutor.scala
@@ -115,7 +115,8 @@ private[akka] trait BatchingExecutor extends Executor {
 
   /** Override this to define which runnables will be batched. */
   def batchable(runnable: Runnable): Boolean = runnable match {
-    case b: Batchable ⇒ b.isBatchable
-    case _            ⇒ false
+    case b: Batchable                           ⇒ b.isBatchable
+    case _: scala.concurrent.OnCompleteRunnable ⇒ true
+    case _                                      ⇒ false
   }
 }
